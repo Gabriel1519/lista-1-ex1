be31b58b89c46d79cc0ad0dd9abdacb483e41ea1
Gabriel1519 <130614709+Gabriel1519@users.noreply.github.com>
2023-04-12 20:20:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int B;
+            int h;
+            int a;
+            Console.Write("Insira a base do Retângulo: ");
+            B = int.Parse(Console.ReadLine());
+            Console.Write("Insira a altura do Retângulo: ");
+            h = int.Parse(Console.ReadLine());
+
+            a = B * h;
+
+            Console.WriteLine("A area do retangulo é {0}.", a);
+
+        }
+    }
+}
